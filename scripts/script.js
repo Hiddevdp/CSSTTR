248a8bf1ee25b09b7473b417adbed2d60ac93344
Hiddevdp <hiddevanderplaat@live.nl>
2024-03-20 16:00:52 +0100

    animation update

---
+++ scripts/script.js
@@ -0,0 +1,31 @@
+const ranges = document.querySelectorAll('[type="range"]');
+
+const updateRange = (range) => {
+  // the name of the custom property is the name of the input
+  const rangeName = range.name;
+  // the value of the custom property is the value of the input
+  const rangeValue = range.value;
+  // a custom property is set on the HTML element
+
+  if (rangeName === "speed") {
+    document.documentElement.style.setProperty(
+      "--" + rangeName,
+      rangeValue + "s"
+    );
+  } else {
+    document.documentElement.style.setProperty(
+      "--" + rangeName,
+      rangeValue + "em"
+    );
+  }
+};
+
+ranges.forEach((range) => {
+  // the custom property is set initially after loading the document
+  updateRange(range);
+
+  // the custom property is updated when the input is changed
+  range.oninput = () => {
+    updateRange(range);
+  };
+});
